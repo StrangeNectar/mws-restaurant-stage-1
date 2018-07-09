07c587900b7fd4317896f739b54c0fc8e6e9569f
Ben Groneman <bluegroneman10@gmail.com>
2018-07-09 15:01:03 -0600

    feat: added a cache for essential items on the page so that offline users can access the page

--- sw.js
+++ sw.js
@@ -1,34 +1,53 @@
-
-// Listens for the initial install of the webpage.
-self.addEventListener('install', function(event) {
-    // Here is what will happen upon initial install of the serverworker/cache
-});
-
-// This listens for when the is a fetch event
-// We should be able to use this for when we reach out for external scripts
-// Or the images, because they are dynamic in nature so we can improve perforance here 
-//self.addEventListener('fetch', function(event) {
-//  event.respondWith(
-//    caches.match(event.request)
-//      .then(function(response) {
-//        if(response) {
-//          return response;
-//        }
-//        console.log(event.request);
-//      }
-//      )
-//  )    
-//})
-
+get CACHE_NAME = 'mws-restaurant-stage-v1';
+let CACHE_URLS = [
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/',
+    '/js/',
+    '/index.html',
+    '/restaurant.html',
+    '/sw.js'
+]
 
 // Listens for the initial install of the webpage.
 // Here is where we will cache everything for the convenience of the offline user
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                '/'
-            ]);
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll([CACHE_URLS]);
         })
     );
 });
+
+// Now that we have cached what we want, lets go ahead and send it out when need be
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(res){
+                if(res) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// Now we need to delete the previous cache and replace it with the updated proper cache
+// This will happen upon the service worker becoming instantiated
+self.addEventListener('activate', function(event){
+    let cacheWhitelist = ['mws-restaurant-stage-v1', 'mws-restaurant-stage-v2'];
+
+    event.waitUntil(
+        caches.keys().the(function(cacheNames){
+            return Promise.all(
+                cacheNames.map(function(cacheName){
+                    if(cacheWhitelist.indexOf(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })        
+            );
+        })        
+    );
+});
+
