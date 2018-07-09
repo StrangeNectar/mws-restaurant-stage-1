3388f42d562fa46f331318ce91125839f1f2ab1c
Ben Groneman <bluegroneman10@gmail.com>
2018-07-09 11:09:48 -0600

    feat: the service worker now will cache all of the necessary files so that the offline user can actually get something

--- sw.js
+++ sw.js
@@ -7,16 +7,28 @@ self.addEventListener('install', function(event) {
 // This listens for when the is a fetch event
 // We should be able to use this for when we reach out for external scripts
 // Or the images, because they are dynamic in nature so we can improve perforance here 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if(response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-      )
-  )    
-})
+//self.addEventListener('fetch', function(event) {
+//  event.respondWith(
+//    caches.match(event.request)
+//      .then(function(response) {
+//        if(response) {
+//          return response;
+//        }
+//        console.log(event.request);
+//      }
+//      )
+//  )    
+//})
 
+
+// Listens for the initial install of the webpage.
+// Here is where we will cache everything for the convenience of the offline user
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                '/'
+            ]);
+        })
+    );
+});
