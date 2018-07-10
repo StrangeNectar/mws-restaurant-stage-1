4dbcc3603d9c4d1d530154f2bc5f46e72cb3eb39
Ben Groneman <bluegroneman10@gmail.com>
2018-07-10 07:39:25 -0600

    fixed the service worker errors

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
-get CACHE_NAME = 'mws-restaurant-stage-v1';
+let CACHE_NAME = 'mws-restaurant-stage-v1';
 let CACHE_URLS = [
     '/css/styles.css',
     '/data/restaurants.json',
-    '/img/',
-    '/js/',
+    '/img/1.jpg',
+    '/img/10.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/sw/index.js'
     '/index.html',
     '/restaurant.html',
     '/sw.js'
