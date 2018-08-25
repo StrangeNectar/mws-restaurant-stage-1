11014fea3de3a16a5de994af759e207103892285
strangenectar <bluegroneman10@gmail.com>
2018-08-24 22:37:22 -0600

    Attempted to fix the indexDB // baby steps

--- bg-idb/bg-idb.js
+++ bg-idb/bg-idb.js
@@ -24,33 +24,40 @@
     dbPromise.then(function(db){
         var tx = db.transaction(restaurantObjectStore, 'readwrite');
         var store = tx.objectStore(restaurantObjectStore);
-        var items = {
-            name: 'sandwich',
-            price: 4.99,
-            description: 'A very tasty sandwich'
-        }; 
-        //let data = fetch(restaurantDataUrl).then(function(response){
-        //    items = response.json();
-        //}).catch(function(err){
-        //    console.log('Our Fetch request for restaurant data has failed: ', err);
-        //});
+        var items = []; 
+
+        let data = fetch(restaurantDataUrl, {
+
+            method: 'GET'
+
+        }).then(function(response){
+             
+            return response.json();
+
+        }).then(function(json){
+            
+						items.push(json["0"]);					
+						console.log('the items are', items);
+
+        }).catch(function(err) {
+
+            console.log('fetch failed');
+
+        }); 
         
         // Once we have populated items with restaurant data we can then add them to the store
 				
-				tx.oncomplete = function(event) {
-					console.log('The transaction has completed');
-				}
-
-				tx.onerror = function(event) {
-					console.log('this sucks')
-				}
-
         store.add(items, 'id');
         return tx.complete;
+
     }).then(function() {
+
         console.log('The items have been added to the object store');
+
     }).catch(function() {
+
         console.log('The items have NOT been added to the store');
+
     }); 
 
 })();
