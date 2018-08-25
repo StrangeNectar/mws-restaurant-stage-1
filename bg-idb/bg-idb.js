5a7440b0a9c1fa0e2e454e1e018344bca4f9b4f8
strangenectar <bluegroneman10@gmail.com>
2018-08-24 20:45:59 -0600

    Hmmm I fixed the indexdb by simply passing the key to the add method. Docs sure are great aren't they

--- bg-idb/bg-idb.js
+++ bg-idb/bg-idb.js
@@ -1,5 +1,9 @@
 (function() {
     'use strict';
+    
+    // Declaring global vars
+    let restaurantDataUrl = 'http://localhost:1337/restaurants/';
+    let restaurantObjectStore = 'restaurantOS';
 
     if(!('indexedDB' in window)) {
         console.log('This browser does not support IndexDB');
@@ -7,29 +11,46 @@
     }
 
     var dbPromise = idb.open('restaurantData-1', 1, function(upgradeDb) {
+
         console.log('creating a new object store');
-        if(!upgradeDb.objectStoreNames.contains('restaurantOS')) {
-            upgradeDb.createObjectStore('restaurantOS', {keyPath: 'id'});
+
+        if(!upgradeDb.objectStoreNames.contains(restaurantObjectStore)) {
+						var restaurantOS = upgradeDb.createObjectStore('restaurants', {keyPath: 'id'});
         }
         // Create more object stores here
     });
 
     // lets catch this promise .then add some data to it
     dbPromise.then(function(db){
-        var tx = db.transaction('restaurantOS', 'readwrite');
-        var store = tx.objectStore('restaurantOS');
-        var items = []; 
-        gatherRestaurantData();
-        console.log('Welp we made it to the promise then function')
+        var tx = db.transaction(restaurantObjectStore, 'readwrite');
+        var store = tx.objectStore(restaurantObjectStore);
+        var items = {
+            name: 'sandwich',
+            price: 4.99,
+            description: 'A very tasty sandwich'
+        }; 
+        //let data = fetch(restaurantDataUrl).then(function(response){
+        //    items = response.json();
+        //}).catch(function(err){
+        //    console.log('Our Fetch request for restaurant data has failed: ', err);
+        //});
+        
+        // Once we have populated items with restaurant data we can then add them to the store
+				
+				tx.oncomplete = function(event) {
+					console.log('The transaction has completed');
+				}
+
+				tx.onerror = function(event) {
+					console.log('this sucks')
+				}
+
+        store.add(items, 'id');
+        return tx.complete;
+    }).then(function() {
+        console.log('The items have been added to the object store');
+    }).catch(function() {
+        console.log('The items have NOT been added to the store');
     }); 
-    
-    // Lets get the data we need
-    function gatherRestaurantData() {
-        for(let i = 0; i < 10; i++) {
-            let dataurl = `http://localhost:1337/restaurants/${i}` 
-            data = fetch(dataurl);
-            console.log(data);
-            return data;
-        }
-    };
+
 })();
