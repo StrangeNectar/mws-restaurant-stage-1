56283001335603e674fc5947e11cef09536dd15f
Ben Groneman <bluegroneman10@gmail.com>
2018-10-24 11:04:02 -0600

    feat: eliminate render-blocking resource

--- sw.js
+++ sw.js
@@ -6,13 +6,23 @@
 var CACHE_NAME = 'mws-restaurant-cache-v1';
 
 var urlsToCache = [
-    '/',
-    '/restaurant.html',
-    'js/dbhelper.js',
-    'js/main.js',
-    'js/restaurant_info.js',
-    'bg-idb/idb.js',
-    'css/styles.css'
+  '/',
+  '/restaurant.html',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'bg-idb/idb.js',
+  'css/styles.css',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg'
 ];
 
 self.addEventListener('install', function(event) {
